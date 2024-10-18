030eb958e6ebc4c261bb141fae1d1754c7fefe97
Gabriel Rosa <galiro@icloud.com>
2024-10-17 23:18:34 -0400

    fix: update to build properly

--- cmake-build-emscripten.sh
+++ cmake-build-emscripten.sh
@@ -1,8 +1,11 @@
 #!/bin/bash 
 set -e
+
+export CMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -sUSE_SDL=2"
+
 cmake   -S . \
         -B build.emscripten \
-        -DCMAKE_TOOLCHAIN_FILE=${EMSDK}emscripten/main/cmake/Modules/Platform/Emscripten.cmake \
+        -DCMAKE_TOOLCHAIN_FILE=${EMSDK}/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake \
         -DCMAKE_BUILD_TYPE=RelWithDebInfo
 make -C build.emscripten
 
